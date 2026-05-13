6460d87217db53ee56322a4f208e15aec3b1a0cd
SmitKakani7 <smit198.rejoice@gmail.com>
2026-05-13 15:01:39 +0530

    feat: update blog components to utilize marked for HTML rendering and improve code structure

--- src/rendering/blogDetails/index.js
+++ src/rendering/blogDetails/index.js
@@ -1,29 +1,25 @@
-import React from 'react'
-import styles from './blogDetails.module.scss';
-import BlogInformation from './blogInformation';
-import { getImageUrl } from '@/utils/blog';
+import React from "react";
+import styles from "./blogDetails.module.scss";
+import BlogInformation from "./blogInformation";
+import { getImageUrl } from "@/utils/blog";
 
 export default function BlogDetails({ blogData, sidebarBlogs }) {
-    return (
-        <>
-            <div className={styles.detailsLayer}>
-                <div className='container-lg'>
-                    <div className={styles.blogDetails}>
-                        <div className={styles.textgrid}>
-                            <h2>
-                                {blogData?.attributes?.title}
-                            </h2>
-                            <p>
-                                {blogData?.attributes?.shortDescription}
-                            </p>
-                        </div>
-                    </div>
-                    <div className={styles.banner}>
-                        <img src={getImageUrl(blogData)} alt='Banner' />
-                    </div>
-                </div>
+  return (
+    <>
+      <div className={styles.detailsLayer}>
+        <div className="container-lg">
+          <div className={styles.blogDetails}>
+            <div className={styles.textgrid}>
+              <h2>{blogData?.attributes?.title}</h2>
+              <p>{blogData?.attributes?.shortDescription}</p>
             </div>
-            <BlogInformation blogData={blogData} sidebarBlogs={sidebarBlogs} />
-        </>
-    )
+          </div>
+          <div className={styles.banner}>
+            <img src={getImageUrl(blogData)} alt="Banner" />
+          </div>
+        </div>
+      </div>
+      <BlogInformation blogData={blogData} sidebarBlogs={sidebarBlogs} />
+    </>
+  );
 }
